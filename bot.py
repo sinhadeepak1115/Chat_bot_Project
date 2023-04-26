8da83797cedffa2039f29fb77dd6760cef9e3ae1
amankas864 <amankas864@gmail.com>
2023-04-26 18:48:10 +0530

    Final Project

--- bot.py
+++ bot.py
@@ -1,26 +1,35 @@
 import tkinter as tk
+import customtkinter as c
 
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+from cleaner import clean_corpus
 
-chatbot = ChatBot('MyBot')
+CORPUS_FILE = "chat.txt"
+
+chatbot = ChatBot("ChatBot")
+
 trainer = ListTrainer(chatbot)
-trainer.train('chat.txt')
+cleaned_corpus = clean_corpus(CORPUS_FILE)
+trainer.train(cleaned_corpus)
 
 def get_response(msg):
     return str(chatbot.get_response(msg))
 
-root = tk.Tk()
+c.set_appearance_mode("System")  
+c.set_default_color_theme("blue")
+
+root = c.CTk()
 root.title('Chatbot GUI')
-root.geometry('400x500')
+root.geometry('410x250')
+
+input_box = c.CTkTextbox(root, height=20, width=250)
+input_box.grid(row=2, column=1, padx=5, pady=5)
 
 
+output_box = c.CTkTextbox(root, height=200, width=400)
+output_box.grid(row=1, column=1, columnspan=2, padx=5, pady=5)
 
-output_box = tk.Text(root, height=20, width=50)
-output_box.pack()
-
-input_box = tk.Text(root, height=2, width=50)
-input_box.pack()
 
 
 def send_message():
@@ -35,25 +44,26 @@ def send_message():
         output_box.insert(tk.END, 'Bot: ' + output_text + '\n')
 
 
-send_button = tk.Button(root, text='Send', command=send_message)
-send_button.pack()
+
+send_button = c.CTkButton(root, text='Send', command=send_message)
+send_button.grid(row=2, column=2, padx=5, pady=5)
 
 
 def show_login():
-    login_window = tk.Toplevel()
+    login_window = c.CTk()
     login_window.title('Login')
     login_window.geometry('300x150')
 
-    username_label = tk.Label(login_window, text='Username:')
+    username_label = c.CTkLabel(login_window, text='Username:')
     username_label.pack()
 
-    username_entry = tk.Entry(login_window)
+    username_entry = c.CTkEntry(login_window)
     username_entry.pack()
 
-    password_label = tk.Label(login_window, text='Password:')
+    password_label = c.CTkLabel(login_window, text='Password:')
     password_label.pack()
 
-    password_entry = tk.Entry(login_window, show='*')
+    password_entry = c.CTkEntry(login_window, show='*')
     password_entry.pack()
 
     def login():
@@ -61,15 +71,18 @@ def show_login():
         password = password_entry.get()
 
         # Check if the credentials are correct
-        if username == 'admin' and password == 'password':
+        if username in ['al2235','ds6854','ap8764'] and password == '123':
             login_window.destroy()
             root.deiconify()
         else:
-            error_label = tk.Label(login_window, text='Invalid username or password')
+            error_label = c.CTkLabel(login_window, text='Invalid username or password')
             error_label.pack()
 
-    login_button = tk.Button(login_window, text='Login', command=login)
+    login_button = c.CTkButton(login_window, text='Login', command=login)
     login_button.pack()
 
+    login_window.mainloop()
+
     root.withdraw()
-root.mainloop()
+
+show_login()
